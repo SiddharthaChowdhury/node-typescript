439bf522bd3297eb4da92e1122d1f261c002b06d
Siddhartha <siddharthac6@gmail.com>
2019-01-25 02:55:06 +0530

    Added extra line on server start with link to open in browser

--- app/app.ts
+++ app/app.ts
@@ -15,4 +15,5 @@ app.get('/', function (req: express.Request, res: express.Response) {
 
 server.listen(3000, function () {
     console.log('Example app listening on port 3000!');
+    console.log(`Browse https://localhost:3000/ and ignore self signed cert`)
 });
